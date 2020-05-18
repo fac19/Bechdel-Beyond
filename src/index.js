d2aaf7fef927305f0c955551a30d0090d1e9e541
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-05-18 22:53:46 +0100

    setup eslint

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (process.env.NODE_ENV !== 'production') {
+	import('react-axe').then((axe) => {
+		axe(React, ReactDOM, 1000);
+		ReactDOM.render(<App />, document.getElementById('root'));
+	});
+} else {
+	ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
