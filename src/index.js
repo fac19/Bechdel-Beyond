d30df11c0e6f0b8268b66569d8bc384ab5dd146a
Giovanna Aveiro <to.gio@pm.me>
2020-05-21 15:46:33 +0100

    BB-36 #Comment Add navbar and movie cards to home route
    Co-authored-by: chloeh24 <chloeh24@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import * as serviceWorker from './serviceWorker';
 if (process.env.NODE_ENV !== 'production') {
 	import('react-axe').then((axe) => {
 		axe.default(React, ReactDOM, 1000);
-		ReactDOM.render(<App />, document.getElementById('root'));
+	ReactDOM.render(<App />, document.getElementById('root'));
+
 	});
 } else {
 	ReactDOM.render(<App />, document.getElementById('root'));
