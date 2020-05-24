a06d1e28997e09091d932eef4ebc9a5316149011
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-05-24 14:53:52 +0100

    ci for front end

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.js</code> and save to reload test text and test
-					branch as well a?.
+					Edit <code>src/App.js</code> and save to reload
 				</p>
 				<a
 					className="App-link"
