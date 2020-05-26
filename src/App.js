0db79b3120d2663ba78823052604769179cffd23
Chloe <chloeh24@hotmail.com>
2020-05-26 17:47:34 +0100

    BB-46 #COMMENT add mobile stepper and setup review page
    Co-authored-by: jamesj-0 <jamesjhardy95@gmail.com>

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import SignUp from './components/UserTasks/Signup/Signup';
 // import MoviePage from './components/Movie/MoviePage';
 import LogIn from './components/UserTasks/Login/Login';
 // import AboutPage from './components/About/AboutPage';
-// import ReviewPage from './components/Review/ReviewPage';
+import ReviewPage from './components/Review/ReviewPage';
 // import UserPage from './components/UserTasks/UserPage/UserPage';
 
 function App() {
@@ -20,38 +20,6 @@ function App() {
 	const [loggedIn, setLoggedIn] = React.useState(null);
 	const [currentPage, setCurrentPage] = React.useState('HOME');
 
-<<<<<<< HEAD
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <MenuAppBar
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-            accessToken={accessToken}
-          />
-        </header>
-        <Switch>
-          {/* <Route exact path="/" component={AllFilms}></Route> */}
-          <Route
-            exact
-            path="/signup"
-            component={SignupPage}
-            accessToken={accessToken}
-          ></Route>
-          {!accessToken ? (
-            <Route
-              exact
-              path="/login"
-              component={SignIn}
-              accessToken={accessToken}
-            ></Route>
-          ) : (
-            ''
-          )}
-          <Route exact path="/create-review" component={ReviewPage}></Route>
-          {/* {accessToken ? (
-=======
 	return (
 		<Router>
 			<div className="App">
@@ -90,9 +58,18 @@ function App() {
 						}
 					/>
 
-					{/* <Route exact path="/create-review" component={ReviewPage}></Route> */}
+					<Route
+						exact
+						path="/create-review"
+						render={() =>
+							loggedIn ? (
+								<Redirect to="/" />
+							) : (
+								<ReviewPage setLoggedIn={setLoggedIn} />
+							)
+						}
+					/>
 					{/* {accessToken ? (
->>>>>>> master
             <Route exact path="/user-profile" component={UserPage}></Route>
           ) : (
             ''
