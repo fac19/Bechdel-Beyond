e3e94f3078d2731b6adef9ca54b5eccb49c2ad83
James Hardy <55053401+jamesj-0@users.noreply.github.com>
2020-05-29 12:45:06 +0100

    Update AboutPage.js

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -1,11 +1,19 @@
 import { Link as RouterLink } from 'react-router-dom';
 import React from 'react';
 import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+	p: {
+		fontSize: '50px',
+	},
+}));
+
 export default function AboutPage() {
+	const classes = useStyles();
 
 	return (
 		<>
@@ -13,7 +21,7 @@ export default function AboutPage() {
 				Bechdel & Beyond
 			</Typography>
 			<Typography variant="h5" component="h5">
-				A new way to discover, review & share movies
+				A new way toclassName={classes.title} discover, review & share movies
 				that pass the Bechdel Test and go beyond...
 			</Typography>
 			<Button variant="outlined">
