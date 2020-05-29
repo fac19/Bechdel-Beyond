66e0075d1fc239b502c177e6eec29e82bb4546ee
James Hardy <55053401+jamesj-0@users.noreply.github.com>
2020-05-29 12:32:24 +0100

    Update AboutPage.js

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -1,16 +1,9 @@
 import { Link as RouterLink } from 'react-router-dom';
 import React from 'react';
 import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-	p: {
-		fontSize: '50px',
-	},
-}));
 
 export default function AboutPage() {
 
