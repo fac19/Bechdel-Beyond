b2b794fe5eabe4ac66a8f61a3737a59fbb70ef36
Giovanna Aveiro <to.gio@pm.me>
2020-06-14 15:28:03 +0100

    Add submit review test

--- cypress/integration/review.spec.js
+++ cypress/integration/review.spec.js
@@ -1,14 +1,22 @@
 /// <reference types="cypress" />
 
-describe('', () => {
+// Stub fetch object because cypress doesnt handle
+// fetch requests
+import { fetchStub } from '../support/helpers';
+
+describe('Can give review', () => {
 	const testToken =
 		'$2a$10$.4XK5WMk1dTdJIpanxAEZOzmZLArCgWzeTTBSDCNqmjtFP/GHvNce';
+	beforeEach(function () {
+		// register the stub as a local function
+		// 'this.' does not work within arrow functions
+		this.fetchStub = fetchStub();
 
-	beforeEach(() => {
 		cy.window().then((win) => {
 			win.localStorage.setItem('access token', testToken);
 		});
 	});
+	beforeEach(() => {});
 
 	it('should take to movie review form', () => {
 		cy.visit('/film/titanic');
@@ -25,6 +33,7 @@ describe('', () => {
 		cy.get('[data-cy=review-question]').contains(
 			'Are there at least 2 women ?',
 		);
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-back]').should('be.disabled');
 	});
 
@@ -32,34 +41,74 @@ describe('', () => {
 		cy.visit('/film/titanic');
 		cy.get('[data-cy=give-review]').contains('Review').click();
 		cy.url().should('contain', 'create-review/titanic');
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-next]').click();
 		cy.get('[data-cy=review-question]').contains(
 			'Do they talk to each other ?',
 		);
+		cy.get('[data-cy=review-back]').should('be.enabled');
 	});
 
 	it('should be able to answer third question', () => {
 		cy.visit('/film/titanic');
 		cy.get('[data-cy=give-review]').contains('Review').click();
 		cy.url().should('contain', 'create-review/titanic');
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-next]').click();
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-next]').click();
 		cy.get('[data-cy=review-question]').contains(
 			'Do they talk about something other than men ?',
 		);
+		cy.get('[data-cy=review-back]').should('be.enabled');
 	});
 
 	it('should be able to answer final question', () => {
 		cy.visit('/film/titanic');
 		cy.get('[data-cy=give-review]').contains('Review').click();
 		cy.url().should('contain', 'create-review/titanic');
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-next]').click();
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-next]').click();
+		cy.get('[type="radio"]').first().check();
 		cy.get('[data-cy=review-next]').click();
 		cy.get('[data-cy=review-question]').contains(
 			'How satisfied were you with the gender representation?',
 		);
+		cy.get('[data-cy=review-back]').should('be.enabled');
 		cy.get('[data-cy=review-next]').should('be.disabled');
+	});
+
+	it('should submit review', function () {
+		this.fetchStub.resolve({
+			json() {
+				return { response: 'success' };
+			},
+			ok: false,
+		});
+
+		cy.visit('/film/titanic');
+		cy.get('[data-cy=give-review]').contains('Review').click();
+		cy.url().should('contain', 'create-review/titanic');
+		cy.get('[type="radio"]').first().check();
+		cy.get('[data-cy=review-next]').click();
+		cy.get('[type="radio"]').first().check();
+		cy.get('[data-cy=review-next]').click();
+		cy.get('[type="radio"]').first().check();
+		cy.get('[data-cy=review-next]').click();
+		cy.get('[type="radio"]').first().check();
+		cy.get('#outlined-basic').type('Horrible film');
+
+		cy.window().then(function (win) {
+			cy.stub(win, 'fetch')
+				.withArgs('https://apibechdel.herokuapp.com/film/titanic/reviews')
+				.as('fetchReview')
+				.returns(this.fetchStub.promise);
+		});
+		cy.get('button[type=submit]').click();
+		cy.get('@fetchReview').should('be.calledOnce');
+		cy.url().should('contain', '/titanic');
 	});
 
 	it('should show logout button in the menu', () => {
